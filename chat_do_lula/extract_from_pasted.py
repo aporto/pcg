ac5b08bc22c5a95d7fad057f5e45e0f0c8efdf85
Alex Porto <alex.itajuba@gmail.com>
2019-06-15 10:27:56 -0300

    Added statistics

--- chat_do_lula/extract_from_pasted.py
+++ chat_do_lula/extract_from_pasted.py
@@ -14,27 +14,61 @@ with open("pasted.txt") as f:
 
 lula_chat = []
 
+statistics_per_person = {}
+
 last_line = lines[0].strip()
-for line in lines[1:]:
+for idx,line in enumerate(lines[1:]):
     line = line.strip()
     if line[:len(last_line)] == last_line:
         if line.count(' ') < 3:
             continue # Ignore lines with less than 4 words (3 spaces). Probably just someone's name
         name = last_line
         text = line[len(name):].strip()
-        lula_chat.append(name + "\t" + text)
+        try:
+            likes = int(lines[idx+2].strip()) # number of likes appear oh the next line
+        except:
+            likes = 0
+        lula_chat.append((name, text, likes))
+
+        if not name in statistics_per_person.keys():
+            statistics_per_person[name] = {'total_likes':0, 'total_msgs':0}
+
+        statistics_per_person[name]['total_msgs'] += 1
+        statistics_per_person[name]['total_likes'] += likes
+
     last_line = line
 
+# Save to csv format
 with open("lula_chat.csv", "w") as f:
-    for line in lula_chat:
-        f.write("%s\n" % line)
+    #for line in lula_chat:
+    f.write("Nome\tTexto\tLikes\n")
+    for (name, text, likes) in lula_chat:
+        f.write("%s\t%s\t%s\n" % (name, text, likes))
 
+
+# save to html format
 with open("lula_chat.html", "w") as f:
     f.write("<html><head></head><body><table>\n")
-    for line in lula_chat:
-        name, text = line.split("\t")
-        f.write("<tr><td>%s</td><td>%s</td></tr>\n" % (name, text))
+    for (name, text, likes) in lula_chat:
+        #name, text, likes = line.split("\t")
+        f.write("<tr><td>%s</td><td>%s</td><td>%s</td></tr>\n" % (name, text, likes))
     f.write("</table></body></html>\n")
+
+# save statistics
+stats_msgs = {}
+stats_likes = {}
+for name, data in statistics_per_person.iteritems():
+    stats_msgs[name] = data['total_msgs']
+    stats_likes[name] = data['total_likes']
+stats_msgs = sorted(stats_msgs.items(), reverse=True, key=lambda kv: kv[1] )
+stats_likes = sorted(stats_likes.items(), reverse=True, key=lambda kv: kv[1] )
+with open("lula_chat_statistics.csv", "w") as f:
+    f.write("sep=\t\n")
+    f.write("Nome\tMensagens\t\tNome\tLikes\n")
+    #for name, data in statistics_per_person.iteritems():
+    for i in xrange(len(stats_msgs)):
+        f.write("%s\t%s\t\t%s\t%s\n" % (stats_msgs[i][0], stats_msgs[i][1], stats_likes[i][0], stats_likes[i][1])) # data['total_msgs'], data['total_likes']))
+
 
 print "Done!"
 
